66c2ee51d5a192870e70d99f95fe8e1aeb2b5239
Chelsey Dolan <chelseymarie6@gmail.com>
2019-09-04 21:00:41 -0700

    chore: adding template files and structure

---
+++ src/router/index.js
@@ -0,0 +1,19 @@
+﻿import Vue from "vue";
+import VueRouter from "vue-router";
+
+import Grid from "@/views/Grid.vue";
+import List from "@/views/List.vue";
+import Blank from "@/views/Blank.vue";
+Vue.use(VueRouter);
+
+// TODO Web Template Studio: Add routes for your new pages here.
+export default new VueRouter({
+  mode: "history",
+  routes: [
+    { path: "/Blank", component: Blank },
+    { path: "/List", component: List },
+    { path: "/Grid", component: Grid },
+
+    { path:"/", redirect: "/Grid" }
+  ]
+});
